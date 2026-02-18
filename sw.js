fc873247ef294b1c3ad5dcc66dae09328a8395db
zakephotos-sudo <zake.photos@gmail.com>
2026-02-18 11:17:13 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,12 @@
-const CACHE_NAME = "spanish-flashcards-v4";
-
-const urlsToCache = [
-  "index.html",
-  "words.json",
-  "manifest.json"
-];
-
-self.addEventListener("install", event => {
-  self.skipWaiting(); // force new SW to activate
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
-  self.clients.claim(); // take control immediately
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => response)
-      .catch(() => caches.match(event.request))
-  );
+  self.clients.claim();
 });
